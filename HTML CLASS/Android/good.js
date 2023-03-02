5f39a9a27212b135522a8086c777ed1de5eb59be
brownstallon9 <111925404+brownstallon9@users.noreply.github.com>
2023-03-02 14:15:51 +0100

    Yes

---
+++ HTML CLASS/Android/good.js
@@ -0,0 +1,13 @@
+var result = null;
+  console.log(result);
+  var x;
+
+  if(x === null) {
+    console.log("null");
+  } else if(x === undefined) {
+    console.log("undefined");
+  } else {
+    console.log("ok");
+  }
+  throw new Error();
+  console.log("Hello");
---
+++ HTML CLASS/JavaScript/Untitled-1.js
@@ -0,0 +1,2 @@
+var train = ["rice", "beans", "fufu", "yam", "semo"]
+console.log(train[3])
